19703d72e9626d6470c4ff3fc6a3243819f30fb2
Levan Loladze <levan.loladzew@gmail.com>
2023-12-13 19:26:41 +0400

    first commit

---
+++ Travel App/Travel App/Travel_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Travel_AppApp.swift
+//  Travel App
+//
+//  Created by Levan Loladze on 13.12.23.
+//
+
+import SwiftUI
+
+@main
+struct Travel_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
